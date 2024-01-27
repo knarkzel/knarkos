0c748ca5802e2efa8b9bd1f829ba51e3a21d2808
Knarkzel <knarkzel@gmail.com>
2024-01-27 20:47:21 +0100

    lib

--- src/init.rs
+++
@@ -1,5 +0,0 @@
-use crate::*;
-
-pub fn main() {
-    println!("Hello, world!");
-}
--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,32 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(knarkos::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod init;
-mod vga;
-mod serial;
+use knarkos::println;
+use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    println!("Hello World!");
+
     #[cfg(test)]
     test_main();
 
-    // Run main
-    init::main();
-    
     loop {}
 }
 
 /// This function is called on panic.
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
-#[cfg(not(test))] // new attribute
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(info: &core::panic::PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
 
-// our panic handler in test mode
 #[cfg(test)]
 #[panic_handler]
-fn panic(info: &core::panic::PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
+fn panic(info: &PanicInfo) -> ! {
+    knarkos::test_panic_handler(info)
 }
