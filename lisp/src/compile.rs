6c85bb72202e1767d95a490c80e24afe20791ed4
Knarkzel <knarkzel@gmail.com>
2024-01-30 01:17:57 +0100

    recursive function

--- lisp/src/compile.rs
+++ lisp/src/compile.rs
@@ -1,6 +1,6 @@
 use crate::{Atom, Expr, Operator};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy)]
 pub enum Instruction {
     Add,
     Push(usize),
@@ -13,47 +13,56 @@ pub struct VirtualMachine {
 }
 
 impl VirtualMachine {
+    fn compile(expr: &Expr) -> Vec<Instruction> {
+        let mut output = Vec::new();
+        match expr {
+            Expr::Constant(Atom::Number(number)) => {
+                output.push(Instruction::Push(*number));   
+            }
+            Expr::Call(function, args) => {
+                // Push all constants to code
+                for arg in args {
+                    match arg {
+                        Expr::Constant(Atom::Number(number)) => {
+                            output.push(Instruction::Push(*number));
+                        },
+                        Expr::Call(_, _) => {
+                            output.extend(Self::compile(arg));
+                        },
+                        _ => panic!("Invalid argument: {arg}"),
+                    }
+                }
+
+                // Push (n - 1) function instruction
+                let instruction = match function.as_ref() {
+                    Expr::Constant(Atom::Operator(Operator::Plus)) => {
+                        Instruction::Add
+                    },
+                    _ => panic!("Invalid function: {function}"),
+                };
+                for _ in 0..args.len() - 1 {
+                    output.push(instruction);
+                }
+            }
+            _ => panic!("Invalid expression: {expr}"),
+        }
+        output
+    }
+
     /// Takes AST and compiles it into code
     pub fn new(ast: &[Expr]) -> Self {
         let mut code = Vec::new();
         for expr in ast {
-            match expr {
-                Expr::Constant(Atom::Number(number)) => {
-                    code.push(Instruction::Push(*number));
-                }
-                Expr::Call(function, args) => {
-                    // Push all constants to code
-                    for arg in args {
-                        match arg {
-                            Expr::Constant(Atom::Number(number)) => {
-                                code.push(Instruction::Push(*number));
-                            },
-                            _ => panic!("Invalid argument: {arg}"),
-                        }
-                    }
-
-                    // Push (n - 1) function instruction
-                    let instruction = match function.as_ref() {
-                        Expr::Constant(Atom::Operator(Operator::Plus)) => {
-                            Instruction::Add
-                        },
-                        _ => panic!("Invalid function: {function}"),
-                    };
-                    for _ in 0..args.len() - 1 {
-                        code.push(instruction.clone());
-                    }
-                },
-                _ => panic!("Invalid expression: {expr}"),
-            }
+            let instructions = Self::compile(expr);
+            code.extend(instructions);
         }
         Self {
             code,
             stack: Vec::new(),
         }
     }
-
-    pub fn run(&mut self) -> Option<usize> {
-        self.stack = Vec::new();
+    
+    pub fn run(mut self) -> Option<usize> {
         for instruction in &self.code {
             match instruction {
                 Instruction::Add => {
--- lisp/src/main.rs
+++ lisp/src/main.rs
@@ -1,8 +1,8 @@
 use lisp::{parse_expr, VirtualMachine};
 
 fn main() {
-    let (_, ast) = parse_expr("(+ 1 2 3)").unwrap();
-    let mut vm = VirtualMachine::new(&[ast]);
+    let (_, ast) = parse_expr("(+ 1 2 3 (+ 4 5))").unwrap();
+    let vm = VirtualMachine::new(&[ast]);
     let output = vm.run();
     println!("Output is {output:?}");
 }
